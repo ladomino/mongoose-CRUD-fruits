974c2e5c2ef4d8ffaf2366933e2815bf72c27c04
ldomino <domino.laurie@gmail.com>
2022-03-09 13:56:22 -0600

    Added Edit and Delete

--- server.js
+++ server.js
@@ -100,6 +100,42 @@ app.post("/fruits", (req, res) => {
     });
 });
 
+//update route
+app.put("/fruits/:id", (req, res) => {
+    // get the id from params
+    const id = req.params.id;
+    // check if the readyToEat property should be true or false
+    req.body.readyToEat = req.body.readyToEat === "on" ? true : false;
+    // update the fruit
+    Fruit.findByIdAndUpdate(id, req.body, { new: true })
+      .then((fruit) => {
+        // redirect to main page after updating
+        res.redirect("/fruits");
+      })
+      // send error as json
+      .catch((error) => {
+        console.log(error);
+        res.json({ error });
+      });
+  });
+  
+// edit route
+app.get("/fruits/:id/edit", (req, res) => {
+    // get the id from params
+    const id = req.params.id;
+    // get the fruit from the database
+    Fruit.findById(id)
+      .then((fruit) => {
+        // render edit page and send fruit data
+        res.render("fruits/edit.liquid", { fruit });
+      })
+      // send error as json
+      .catch((error) => {
+        console.log(error);
+        res.json({ error });
+      });
+  });
+
 // show route
 app.get("/fruits/:id", (req, res) => {
     // get the id from params
@@ -119,6 +155,22 @@ const fruitId = req.params.id;
     });
 })
 
+// Delete Route
+app.delete("/fruits/:id", (req, res) => {
+    // get the id from params
+    const id = req.params.id;
+    // delete the fruit
+    Fruit.findByIdAndRemove(id)
+      .then((fruit) => {
+        // redirect to main page after deleting
+        res.redirect("/fruits");
+      })
+      // send error as json
+      .catch((error) => {
+        console.log(error);
+        res.json({ error });
+      });
+  });
 
 
 //////////////////////////////////////////////
