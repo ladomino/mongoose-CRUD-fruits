b5b664823ec9e95b03c4c8d7ee246242d638953a
ldomino <domino.laurie@gmail.com>
2022-03-09 09:53:51 -0600

    Initial server setup

---
+++ server.js
@@ -0,0 +1,36 @@
+/////////////////////////////////////////////
+// Import Our Dependencies
+/////////////////////////////////////////////
+require("dotenv").config(); // Load ENV Variables
+const express = require("express"); // import express
+const morgan = require("morgan"); //import morgan
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+const path = require("path")
+
+
+////////////////////////////////////////////////
+// Create our Express Application Object Bind Liquid Templating Engine
+/////////////////////////////////////////////////
+const app = require("liquid-express-views")(express());
+
+/////////////////////////////////////////////////////
+// Middleware
+/////////////////////////////////////////////////////
+app.use(morgan("tiny")); //logging
+app.use(methodOverride("_method")); // override for put and delete requests from forms
+app.use(express.urlencoded({ extended: false})); // parse urlencoded request bodies
+app.use(express.static("public")); // serve files from public statically
+
+////////////////////////////////////////////
+// Routes
+////////////////////////////////////////////
+app.get("/", (req, res) => {
+    res.send("your server is running... better catch it.");
+});
+
+//////////////////////////////////////////////
+// Server Listener
+//////////////////////////////////////////////
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`));
